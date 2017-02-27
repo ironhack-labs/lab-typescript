fd8ada59465624e2b07e030b3479bf8209551f20
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-02-27 17:01:08 +0100

    done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,28 @@
-"use strict";
+class Todo {
+    constructor() {
+        this.tasks = [];
+    }
+    addTask(task) {
+        this.tasks.push(task);
+        return this.tasks.length;
+    }
+    deleteTask(task) {
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+        return this.tasks.length;
+    }
+    listAllTasks() {
+        this.tasks.forEach((task) => {
+            console.log(task);
+        });
+    }
+}
+let myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -2,6 +2,30 @@
 
 // 1. Create a class Todo that implements the Interface created before.
 
+import { Tasks } from  "./todoInterface.js";
+
+class Todo implements Tasks {
+  tasks: Array<string> = [];
+
+  constructor(){}
+
+  addTask(task: string){
+    this.tasks.push(task);
+    return this.tasks.length;
+  }
+
+  deleteTask(task: string){
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+    return this.tasks.length;
+  }
+
+  listAllTasks(){
+    this.tasks.forEach((task) => {
+      console.log(task);
+    })
+  }
+}
+
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
