192bc02970b4d06374d6c5f4940227659062d093
Carlos Luis <clburguete@gmail.com>
2017-05-15 15:48:02 +0200

    done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,30 @@
 "use strict";
+exports.__esModule = true;
+var Todo = (function () {
+    function Todo(List) {
+        if (List === void 0) { List = []; }
+        this.List = List;
+    }
+    Todo.prototype.addTask = function (task) {
+        this.List.push(task);
+        return this.List.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        console.log(this.List);
+    };
+    Todo.prototype.deleteTask = function (task) {
+        var toRemove = this.List.indexOf(task);
+        this.List.splice(toRemove, 1);
+    };
+    return Todo;
+}());
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -3,3 +3,11 @@
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive a string, doesn't return anything.
+interface TodoInterface {
+  List: Array<string>;
+  addTask(task: string): number;
+  listAllTasks(): void;
+  deleteTask(task: string): void;
+}
+
+module.exports = TodoInterface;
