371a886e064cd43aa04ad1fd5ef5a9601a343151
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-06-13 17:37:08 -0400

    done

--- starter-code/iteration-3/interfaces.ts
+++ starter-code/iteration-3/interfaces.ts
@@ -1,12 +1,26 @@
 // Write the interface for class TodoItem. It must have:
+interface ToItem {
+
 // - A title of the task as a property.
+ tittle:string;
 // - A boolean to mark if it's done.
+status:boolean;
 // - A property updatedAt, obiously is a date.
+updatedAt:number;
 // - Method for toogle if it's done or not, doesn't return anything.
+onORoff():void;
+}
 
 // Write the interface for class Todo. It must have:
-// - An array of TodoItem as a property.
-// - Method for adding task where will receive an instance of TodoItem, doesn't return anything.
+interface To{
+  // - An array of TodoItem as a property.
+  lists:Array<string>;
+  // - Method for adding task where will receive an instance of TodoItem, doesn't return anything.
+addTask(name):void;
 // - Method for listing all task in the console, doesn't return anything.
+listAllTasks():void;
 // - Method for delete a task where will receive an instance of TodoItem, doesn't return anything.
-// - Method for show all tasks that are not yet done, doesn't return anything.
+deleteTask(name):void;
+// - Method for showw all tasks that are not yet done, doesn't return anything.
+listUncomplete():void;
+}
--- starter-code/iteration-3/main.ts
+++ starter-code/iteration-3/main.ts
@@ -1,25 +1,89 @@
 // Add the reference to the interface
 
 // Create class TodoItem that implements the corresponding interface
+class TodoItem implements ToItem {
 
+  public tittle:string;
+
+  public status:boolean = true;
+
+
+  public  updatedAt:number;
+constructor(tite:string,updat:number = Date.now()){
+  this.tittle = tite;
+  this.updatedAt = updat;
+};
+
+public onORoff():void{
+  if(this.status === true){
+    this.status = false;
+    this.updatedAt = Date.now();
+  }
+
+};
+
+}
 // Create class TodoList that implements the corresponding interface
+class TodoList implements To{
+
+  public lists:Array<string>=[];
+
+  public addTask(name):void{
+    console.log(this.lists)
+    this.lists.push(name);
+    console.log(`Item ${this.lists.indexOf(name)} --> ${name.tittle}`)
+  };
+
+  public listAllTasks():void{
+this.lists.forEach((one)=>{
+  console.log(`Item ${this.lists.indexOf(one)} --> ${one.tittle}`);
+
+});
+
+  };
+
+  public deleteTask(name):void{
+
+    console.log(`Item ${this.lists.indexOf(name)} --> ${name.tittle}`)
+    if(this.lists.indexOf(name) > -1 ){
+      name.onORoff();
+
+
+    }
+
+  };
+
+  public listUncomplete():void{
+
+this.lists.forEach((one)=>{
+if(one.status){
+  console.log(`Item ${this.lists.indexOf(one)} --> ${one.tittle}`);
+}
+
+});
+  };
+}
+
+
+
 
 // Execution
 let task1 = new TodoItem('This is our first task');
+console.log(task1)
 let task2 = new TodoItem('Eat pizza 🍕 yummy!!!');
 let task3 = new TodoItem('Finish this iteration 1!! 🤓');
 let task4 = new TodoItem('Finish this iteration 2!! 🤓');
 let task5 = new TodoItem('Finish this iteration 3!! 🤓');
 
-let myTodos = new TodoList();
+let myTodo = new TodoList();
 
-console.log("Number of items:", myTodos.addTask(task1));
-console.log("Number of items:", myTodos.addTask(task2));
-console.log("Number of items:", myTodos.addTask(task3));
-console.log("Number of items:", myTodos.addTask(task4));
-console.log("Number of items:", myTodos.addTask(task5));
-myTodos.listAllTasks();
-console.log("Number of items:", myTodos.deleteTask(task3));
-console.log("Number of items:", myTodos.deleteTask(task4));
-console.log("Number of items:", myTodos.deleteTask(task5));
-myTodos.listUncomplete();
+console.log("Number of items:", myTodo.addTask(task1));
+console.log("Number of items:", myTodo.addTask(task2));
+console.log("Number of items:", myTodo.addTask(task3));
+console.log("Number of items:", myTodo.addTask(task4));
+console.log("Number of items:", myTodo.addTask(task5));
+myTodo.listAllTasks();
+console.log("Number of items:", myTodo.deleteTask(task3));
+console.log("Number of items:", myTodo.deleteTask(task4));
+console.log("Number of items:", myTodo.deleteTask(task5));
+myTodo.listUncomplete();
