4955a7de58f6f319287da328ce988029b3d27168
Lawrence Fitzgerald <lfitzmail@gmail.com>
2017-07-11 14:47:11 -0400

    Project update.
    Completed iteration one and some of iteration two.  Could not finish before time to submit.

--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -3,3 +3,29 @@
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive a string, doesn't return anything.
+
+
+interface ToDoTasks {
+  taskArray: string[];
+  addTask(task: string): number;
+  listAllTasks();
+  deleteTask(task: string): number;
+}
+
+class Task implements ToDoTasks {
+  taskArray: string[] = [ 'Complete assignment', 'Eat lunch' ];
+  addTask(newTask: string) {
+    taskArray.push(newTask);
+    return taskArray.length,
+
+    console.log("============= NEW TASK =============="),
+    console.log("Task: " + newTask + "inserted in the list"),
+    console.log("Number of items: " + taskArray.length);
+  }
+
+  listAllTasks() {
+    // this.taskArray = taskArray;
+    taskArray.forEach((eachTask) => {
+    console.log(eachTask);
+  }
+}
