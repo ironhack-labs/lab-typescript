9f4e6db61b1592c98195da7d273d79aaa5e7a5bb
Carlos Dueñas <carlosdss84@gmail.com>
2017-07-24 17:05:09 +0200

    iteration 3 hecha

--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,9 +1,33 @@
+
 // Add the reference to the "TodoInterface"
+import { toDoInterface } from "./todoInterface";
 
 // 1. Create a class Todo that implements the Interface created before.
+class TodoList implements toDoInterface {
+  toDos = [];
+  constructor() {}
 
+  addTask(toDo: String) {
+    this.toDos.push(toDo);
+    console.log ("Se ha añadido la tarea " + toDo);
+    return this.toDos.length;
+  }
+
+
+  listAllTasks() {
+    this.toDos.forEach(todo => {
+      console.log(todo);
+    });
+  }
+  deleteTask(toDoToDelete: String) {
+    let index = this.toDos.indexOf(toDoToDelete);
+    console.log ("Se ha eliminado la tarea " + this.toDos[index]);
+    this.toDos.splice(index, 1);
+    return this.toDos.length;
+  }
+}
 // Execution
-let myTodos = new Todo();
+let myTodos = new TodoList();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
--- starter-code/iteration-3/interfaces.ts
+++ starter-code/iteration-3/interfaces.ts
@@ -10,3 +10,19 @@
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive an instance of TodoItem, doesn't return anything.
 // - Method for show all tasks that are not yet done, doesn't return anything.
+
+import { TodoItem } from "./main";
+
+export interface TodoItemInterface {
+  title: string;
+  status: boolean;
+  updatedAt: Date;
+  toogleStatus():void;
+}
+
+export interface TodoInterface {
+    items: Array<TodoItem>;
+    addTask(item:TodoItem):Number;
+    listAllTasks();
+    deleteTask(item:TodoItem):Number;
+  }
