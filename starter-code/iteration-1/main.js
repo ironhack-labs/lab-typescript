bf8610fded6846dc315d0a9a5de6bf8ee5bf98d4
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-24 15:25:58 +0200

    iterate through array in iteration 1

--- starter-code/iteration-1/main.js
+++ starter-code/iteration-1/main.js
@@ -1,6 +1,17 @@
-var arrayOfStrings = [];
+var arrayOfTasks = [];
 function addTask(task) {
     console.log("a new task is added and the new array lenght is: ");
-    return arrayOfStrings.push(task);
+    return arrayOfTasks.push(task);
+}
+function listAllTasks() {
+    for (var _i = 0, arrayOfTasks_1 = arrayOfTasks; _i < arrayOfTasks_1.length; _i++) {
+        var task = arrayOfTasks_1[_i];
+        console.log("Task listed : ", task);
+    }
 }
 console.log("Number of items:", addTask('This is our first task'));
+console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
+listAllTasks();
--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,24 +1,28 @@
 // Iteration 1
 // 1. Create an array of strings
-let arrayOfStrings: string[] = [];
-
-function addTask(task: string):number {
-  console.log("a new task is added and the new array lenght is: ")
-  return arrayOfStrings.push(task);
-}
+let arrayOfTasks: string[] = [];
 
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
+function addTask(task: string):number {
+  console.log("a new task is added and the new array lenght is: ")
+  return arrayOfTasks.push(task);
+}
 // 3. Create a function to list all tasks, it must show in the console de task.
+
+function listAllTasks():void {
+    for(var task of arrayOfTasks){
+      console.log("Task listed : ", task);
+    }
+}
 
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
 
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
-// console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
-// console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
-// console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
-// console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
-// listAllTasks();
+console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
+listAllTasks();
 // console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
 // listAllTasks();
