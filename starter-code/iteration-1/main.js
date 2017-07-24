d9c919ff2106e0a92084316cc6132cd0ecd8b43a
Mario Martin <mariomarsan7@gmail.com>
2017-07-24 15:24:26 +0200

    Add changes in iteration 1

--- starter-code/iteration-1/main.js
+++ starter-code/iteration-1/main.js
@@ -1,3 +1,15 @@
+var taskList = [];
+function addTask(task) {
+    taskList.push(task);
+}
+function listAllTasks() {
+    taskList.forEach(function (e) {
+        console.log(e);
+    });
+}
+function deleteTask(task) {
+    taskList = taskList.filter(function (e) { return e !== task; });
+}
 console.log("Number of items:", addTask('This is our first task'));
 console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
