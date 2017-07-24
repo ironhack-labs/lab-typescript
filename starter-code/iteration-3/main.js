e55146a81b1dc67203a2dcc2d8fab6e8d1e7d33b
Rachel Renee Flores <rachelreneeflores@gmail.com>
2017-07-24 17:05:24 +0200

    Done

--- starter-code/iteration-3/main.js
+++ starter-code/iteration-3/main.js
@@ -1,9 +1,55 @@
+"use strict";
+exports.__esModule = true;
+var TodoItem = (function () {
+    function TodoItem(title) {
+        this.title = title;
+        this.status = false;
+        this.updatedAt = new Date();
+    }
+    TodoItem.prototype.togglestatus = function () {
+        this.status = !this.status;
+        this.updatedAt = new Date();
+    };
+    return TodoItem;
+}());
+var TodoList = (function () {
+    function TodoList(tasks) {
+        this.tasks = tasks;
+    }
+    TodoList.prototype.addTask = function (task) {
+        this.tasks.push(task);
+        console.log("=========== NEW TASK ===========\n  Task \"" + task.title + "\" inserted in the list");
+        return this.tasks.length;
+    };
+    TodoList.prototype.listAllTasks = function () {
+        this.tasks.forEach(function (task) {
+            console.log(task.title);
+        });
+    };
+    TodoList.prototype.deleteTask = function (task) {
+        var index = this.tasks.indexOf(task);
+        this.tasks.splice(index, 1);
+        console.log("=========== TASK REMOVED ===========\n  Task \"" + task.title + "\" removed from the list");
+        return this.tasks.length;
+    };
+    TodoList.prototype.listUncomplete = function () {
+        var _this = this;
+        {
+            this.tasks.forEach(function (task) {
+                if (_this.status === false) {
+                    console.log(task.title);
+                }
+            });
+        }
+    };
+    return TodoList;
+}());
 var task1 = new TodoItem('This is our first task');
 var task2 = new TodoItem('Eat pizza 🍕 yummy!!!');
 var task3 = new TodoItem('Finish this iteration 1!! 🤓');
 var task4 = new TodoItem('Finish this iteration 2!! 🤓');
 var task5 = new TodoItem('Finish this iteration 3!! 🤓');
-var myTodos = new TodoList();
+var myTodos = new TodoList([]);
 console.log("Number of items:", myTodos.addTask(task1));
 console.log("Number of items:", myTodos.addTask(task2));
 console.log("Number of items:", myTodos.addTask(task3));
