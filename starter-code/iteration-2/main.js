963e108532a0d2d08aca664053b5ab2525b3958f
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-09-25 17:53:25 +0200

    iteration 2 done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,3 +1,25 @@
+var Todo = (function () {
+    function Todo() {
+        this.TodoList = [];
+    }
+    Todo.prototype.addTask = function (arg) {
+        this.TodoList.push(arg);
+        console.log("Task added!");
+        return this.TodoList.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        for (var i = 0; i < this.TodoList.length; i++) {
+            console.log(this.TodoList[i]);
+        }
+    };
+    ;
+    Todo.prototype.deleteTask = function (arg) {
+        this.TodoList.splice(this.TodoList.indexOf(arg), 1);
+        console.log("Item deleted!");
+        return this.TodoList.length;
+    };
+    return Todo;
+}());
 var myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,7 +1,24 @@
 // Add the reference to the "TodoInterface"
-
+/// <referencepath="./TodoInterface"/>
 // 1. Create a class Todo that implements the Interface created before.
-
+class Todo implements TodoInterface {
+  TodoList: Array<string> = [];
+  addTask(arg: string): number {
+    this.TodoList.push(arg)
+    console.log("Task added!");
+    return this.TodoList.length;
+  }
+  listAllTasks(){
+    for (var i = 0; i < this.TodoList.length; i++) {
+        console.log(this.TodoList[i]);
+    }
+  };
+  deleteTask(arg: string): number {
+    this.TodoList.splice(this.TodoList.indexOf(arg), 1);
+    console.log("Item deleted!");
+    return this.TodoList.length;
+  }
+}
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
