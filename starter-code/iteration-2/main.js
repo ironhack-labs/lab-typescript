5b65bd66f0038885ac392fdfdb98cab117c83de1
Michael Rendon <mrr71294@gmail.com>
2017-11-09 10:39:59 -0500

    finished iteration 2

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,3 +1,32 @@
+"use strict";
+exports.__esModule = true;
+var Todo = (function () {
+    function Todo() {
+    }
+    Todo.prototype.addTask = function (task) {
+        taskList.push(task);
+        console.log("You've added \"" + task + "\" to your To Do List!");
+        return taskList.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        for (var i = 0; i < taskList.length; i++) {
+            console.log(taskList[i]);
+        }
+    };
+    Todo.prototype.deleteTask = function (task) {
+        var toBeDeleted = taskList.indexOf(task);
+        if (toBeDeleted === -1) {
+            console.log("That is not a valid task, please check your spelling!");
+        }
+        else {
+            taskList.splice(toBeDeleted, 1);
+            console.log("You've deleted \"" + task + "\" from your To Do List!");
+            return taskList.length;
+        }
+    };
+    return Todo;
+}());
+;
 var myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
