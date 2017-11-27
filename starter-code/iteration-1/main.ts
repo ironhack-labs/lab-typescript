ed16ee3e09db365f590bbfd0edc0286366d66ab9
Dafne <olca.dafne@gmail.com>
2017-11-27 15:37:28 +0100

    iteration 1 done

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,18 +1,37 @@
 // Iteration 1
 // 1. Create an array of strings
+let myTasks = [];
 
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
+let addTask = function(task: string): number {
+  console.log("Add element to myTasks array");
+  myTasks.push(task);
+  return myTasks.length;
+};
 
 // 3. Create a function to list all tasks, it must show in the console de task.
 
+let listAllTasks = function(myArray: Array<String>): void {
+  for (let i = 0; i <= myArray.length; i++) {
+    console.log(myArray[i]);
+  }
+};
+
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
 
+let deleteTask = function(deletedTasks: string): number {
+  let index = myTasks.indexOf(deletedTasks);
+  console.log("task " + myTasks[index] + " has been deleted.");
+  myTasks.splice(index, 1);
+  return myTasks.length;
+};
+
 // Execution
-console.log("Number of items:", addTask('This is our first task'));
-console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
-console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
-console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
-listAllTasks();
-console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
-listAllTasks();
+console.log("Number of items:", addTask("This is our first task"));
+console.log("Number of items:", addTask("Eat pizza 🍕 yummy!!!"));
+console.log("Number of items:", addTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", addTask("Finish this iteration 2!! 🤓"));
+console.log("Number of items:", addTask("Finish this iteration 3!! 🤓"));
+listAllTasks(myTasks);
+console.log("Number of items:", deleteTask("Finish this iteration 1!! 🤓"));
+listAllTasks(myTasks);
