28c38e653024be637947087b07ee1775803e6e71
Sara <saraeme20@gmail.com>
2017-11-27 14:43:25 +0100

    iteration 2 done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,4 +1,32 @@
-var myTodos = new Todo();
+class Todo {
+    constructor(tasks) {
+        this.tasks = tasks;
+    }
+    addTask(task) {
+        this.tasks.push(task);
+        console.log("=========== NEW TASK ===========");
+        console.log("Task " + task + " inserted in the list");
+        console.log("Number of items:" + this.tasks.length);
+        return this.tasks.length;
+    }
+    ;
+    listAllTasks() {
+        this.tasks.forEach(e => {
+            console.log(e);
+        });
+    }
+    ;
+    deleteTask(task) {
+        let pos = this.tasks.indexOf(task);
+        this.tasks.splice(pos, 1);
+        console.log("=========== TASK REMOVED ===========");
+        console.log("Task " + task + " removed from the list");
+        console.log("Number of items:" + this.tasks.length);
+        return this.tasks.length;
+    }
+    ;
+}
+let myTodos = new Todo([]);
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
