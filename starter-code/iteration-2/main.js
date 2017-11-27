ad3d5800024a237f5303ed972f67f9de9b481b04
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-27 16:02:15 +0100

    Iteration 2 complete

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,4 +1,20 @@
-var myTodos = new Todo();
+class Todo {
+    constructor() {
+        this.myList = [];
+    }
+    addTask(task) {
+        this.myList.push(task);
+        return this.myList.length;
+    }
+    listAllTasks() {
+        this.myList.forEach(e => console.log(e));
+    }
+    deleteTask(taskToDelete) {
+        this.myList.splice(this.myList.indexOf(taskToDelete), 1);
+        return this.myList.length;
+    }
+}
+let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,7 +1,26 @@
 // Add the reference to the "TodoInterface"
-
+import { List } from "./todoInterface";
 // 1. Create a class Todo that implements the Interface created before.
-
+class Todo implements List {
+  myList = [];
+  constructor(){}
+  // 1. Create an array of strings
+  // 2. Create a function to add a task in to the array, call it `addTask`,
+  // the function has to accept a string and don't have to return anything.
+  addTask(task:string){
+    this.myList.push(task);
+    return this.myList.length;
+  }
+  // 3. Create a function to list all tasks, it must show in the console de task.
+  listAllTasks():void{
+    this.myList.forEach(e => console.log(e));
+  }
+  // 4. Create a function to delete a task, you must find the task inside the array and delete it.
+  deleteTask(taskToDelete:string){
+    this.myList.splice(this.myList.indexOf(taskToDelete),1);
+    return this.myList.length;
+  }
+}
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
