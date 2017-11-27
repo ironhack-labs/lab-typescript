08207d38081b0216219e7b6705d8598e84e72b9f
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-27 15:39:04 +0100

    iteration 2

--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,7 +1,27 @@
 // Add the reference to the "TodoInterface"
+interface Todos {
+  tasks: Array<string>;
+  addTask(task: string): number;
+  listAllTasks();
+  deleteTask(task: string): number;
+};
 
 // 1. Create a class Todo that implements the Interface created before.
-
+class Todo implements Todos {
+  constructor(public tasks: Array<string>) {};
+  addTask(task) {
+    this.tasks.push(task);
+  }
+  listAllTasks(){
+    this.tasks.forEach(task => {
+      console.log(task);
+    });
+  }
+  deleteTask(task) {
+    const i = this.tasks.indexOf(task);
+    this.tasks.splice(i, 1);
+  }
+}
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
