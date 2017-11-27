58747e49261894a74b631e31d8751125e31745e0
Jean Nothomb <jeannothomb@gmail.com>
2017-11-27 15:15:15 +0100

    first iteration done

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,11 +1,33 @@
 // Iteration 1
 // 1. Create an array of strings
 
+let tasksToComplete: Array<string> = [];
+
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
+
+function addTask(public task: string) {
+    tasksToComplete.push(task);
+    console.log("Task\"" + task + "\"inserted in the list")
+    console.log("Number of items: " + tasksToComplete.length)
+}
 
 // 3. Create a function to list all tasks, it must show in the console de task.
 
+function listAllTasks(tasksToComplete) {
+    tasksToComplete.forEach(function (task) {
+        console.log(task)
+    });
+}
+
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
+
+function deleteTask(task: string) {
+    let taskIndex = tasksToComplete.indexOf(task);
+    tasksToComplete.splice(taskIndex, 1);
+    return tasksToComplete.length;
+    console.log("Task \"" + task + "\" removed from the list");
+    console.log("Number of items: " + tasksToComplete.length);
+}
 
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
