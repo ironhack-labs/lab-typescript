666ae5f5cecb0b947238b96343989ee19f600fe1
Sara <saraeme20@gmail.com>
2017-11-27 17:14:20 +0100

    done

--- starter-code/iteration-3/main.ts
+++ starter-code/iteration-3/main.ts
@@ -1,8 +1,49 @@
 // Add the reference to the interface
+import { listItemInterface , toDoListInterface} from './interfaces';
 
-// Create class TodoItem that implements the corresponding interface
+class TodoItem implements listItemInterface {
+  constructor(
+    public title: String,
+    public status: boolean = false,
+    public updatedAt: Date = new Date()) { }
+
+  toogle(): void {
+    this.updatedAt = new Date();
+    if (this.status === false) {
+      this.status = true;
+    }
+    else this.status = false;
+  };
+}
 
 // Create class TodoList that implements the corresponding interface
+class TodoList implements toDoListInterface {
+  constructor(public tasks: Array<TodoItem>, ) { }
+
+  addTask(task: TodoItem): number {
+    this.tasks.push(task);
+    console.log("=========== NEW TASK ===========");
+    console.log("Task " + task + " inserted in the list");
+    console.log("Number of items:" + this.tasks.length);
+    return this.tasks.length;
+  };
+
+  listAllTasks(): void {
+    this.tasks.forEach(e => {
+      console.log(e.title);
+      console.log(e.updatedAt);
+    });
+  };
+
+  deleteTask(task: listItemInterface): number {
+    let pos: number = this.tasks.indexOf(task);
+    this.tasks.splice(pos, 1);
+    console.log("=========== TASK REMOVED ===========");
+    console.log("Task " + task + " removed from the list");
+    console.log("Number of items:" + this.tasks.length);
+    return this.tasks.length;
+  };
+}
 
 // Execution
 let task1 = new TodoItem('This is our first task');
@@ -11,7 +52,7 @@ let task3 = new TodoItem('Finish this iteration 1!! 🤓');
 let task4 = new TodoItem('Finish this iteration 2!! 🤓');
 let task5 = new TodoItem('Finish this iteration 3!! 🤓');
 
-let myTodos = new TodoList();
+let myTodos = new TodoList([]);
 
 console.log("Number of items:", myTodos.addTask(task1));
 console.log("Number of items:", myTodos.addTask(task2));
@@ -22,4 +63,4 @@ myTodos.listAllTasks();
 console.log("Number of items:", myTodos.deleteTask(task3));
 console.log("Number of items:", myTodos.deleteTask(task4));
 console.log("Number of items:", myTodos.deleteTask(task5));
-myTodos.listUncomplete();
+//myTodos.listUncomplete();
