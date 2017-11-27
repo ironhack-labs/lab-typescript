88501f61ea38e5bf80bda836524d1e9e1443a7b5
Riftland <fran.mscr@gmail.com>
2017-11-27 16:56:35 +0100

    Iteration 2 done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,3 +1,30 @@
+"use strict";
+exports.__esModule = true;
+// 1. Create a class Todo that implements the Interface created before.
+var Todo = /** @class */ (function () {
+    function Todo() {
+    }
+    ;
+    Todo.prototype.addTask = function (task) {
+        this.tasks.push(task);
+        console.log("Tasks inserted: " + this.tasks.length + " | Last: " + task[this.tasks.length - 1]);
+        return this.tasks.length;
+    };
+    ;
+    Todo.prototype.listAllTasks = function () {
+        this.tasks.forEach(function (e) {
+            console.log(e);
+        });
+    };
+    ;
+    Todo.prototype.deleteTask = function (task) {
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+        console.log("Delete successful | Elements: " + this.tasks.length);
+        return this.tasks.length;
+    };
+    return Todo;
+}());
+// Execution
 var myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -1,4 +1,10 @@
 // Write the interface for class Todo. It must have:
+export interface TodoInterface {
+  tasks: Array<string>;
+  addTask(task:string):number;
+  listAllTasks():void;
+  deleteTask(task:string):number;
+}
 // - An array of strings as a property.
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
