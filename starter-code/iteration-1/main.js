371d9ccd95c10d5326e5e8d6fef278170443a2c3
Abraham Castro <cucharadepalo@gmail.com>
2017-11-27 13:01:54 +0100

    Iteration 1

---
+++ starter-code/iteration-1/main.js
@@ -0,0 +1,22 @@
+var tasks = [];
+function addTask(task) {
+    tasks.push(task);
+    return tasks.length;
+}
+function listAllTasks() {
+    tasks.forEach(function (e) {
+        console.log(e);
+    });
+}
+function deleteTask(task) {
+    tasks.splice(tasks.indexOf(task), 1);
+    return tasks.length;
+}
+console.log("Number of items:", addTask('This is our first task'));
+console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
+listAllTasks();
+console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
+listAllTasks();
--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,11 +1,26 @@
 // Iteration 1
 // 1. Create an array of strings
+let tasks: Array<string> = [];
 
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
+function addTask(task : string) {
+  tasks.push(task);
+  return tasks.length;
+}
+
 
 // 3. Create a function to list all tasks, it must show in the console de task.
+function listAllTasks() {
+  tasks.forEach(e => {
+    console.log(e);
+  })
+}
 
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
+function deleteTask(task : string) {
+  tasks.splice(tasks.indexOf(task), 1);
+  return tasks.length;
+}
 
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
