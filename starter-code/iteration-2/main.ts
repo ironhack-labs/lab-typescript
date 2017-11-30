c88f644ae66b53f3dd9d42827122053ea313dd89
Armando <ardo.acosta@gmail.com>
2017-11-30 15:43:31 -0500

    lab

--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,7 +1,31 @@
 // Add the reference to the "TodoInterface"
-
+import {toDoInterface} from "./todointerface";
 // 1. Create a class Todo that implements the Interface created before.
 
+class Todo implements toDoInterface{
+
+  constructor(
+    public myTask: string[] = []
+  )
+
+  {}
+
+    addTask(task){
+
+      this.myTask.push(task);
+
+
+    }
+  listAllTasks(){
+      console.log(this.myTask);
+
+  }
+ deleteTask(task){
+    const index = this.myTask.indexOf(task);
+    this.myTask.splice(index,1);
+
+  }
+  }
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
