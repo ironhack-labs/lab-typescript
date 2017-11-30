f4b59da6e50ae735ae54e3903da4d90c2fa85348
Alejandro Poggi <apoggi419@gmail.com>
2017-11-30 15:39:22 -0500

    on teration 3

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,12 +1,27 @@
 // Iteration 1
 // 1. Create an array of strings
-
+let stringArray: string[] = [];
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
+function addTask (task:string): number {
+  stringArray.push(task);
+  console.log(`${task} inserted into list of tasks`);
+  console.log('Number of tasks: ', stringArray.length);
+  return stringArray.length;
+}
 // 3. Create a function to list all tasks, it must show in the console de task.
-
+function listAllTasks(): void{
+  stringArray.forEach(task => {
+    console.log(task);
+  });
+  return;
+}
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
-
+function deleteTask(task: string): number{
+  const taskIndex: number = stringArray.indexOf(task);
+  stringArray.splice(taskIndex, 1);
+  console.log(`Deleted ${task} at position ${taskIndex} in our task list.`);
+  return stringArray.length;
+}
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
 console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,7 +1,30 @@
 // Add the reference to the "TodoInterface"
-
+import {TodoInterface} from './todoInterface';
 // 1. Create a class Todo that implements the Interface created before.
-
+class Todo implements TodoInterface{
+  stringArray: string[];
+  constructor(){
+    this.stringArray = [];
+  }
+  addTask(task:string):number{
+    this.stringArray.push(task);
+    console.log(`${task} inserted into list of tasks`);
+    console.log('Number of tasks: ', this.stringArray.length);
+    return this.stringArray.length;
+  }
+  listAllTasks():void{
+    this.stringArray.forEach(task => {
+      console.log(task);
+    });
+    return;
+  }
+  deleteTask(task:string):number{
+    const taskIndex: number = this.stringArray.indexOf(task);
+    this.stringArray.splice(taskIndex, 1);
+    console.log(`Deleted ${task} at position ${taskIndex} in our task list.`);
+    return this.stringArray.length;
+  }
+}
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
