ae40e03c02f839d91df8302b575241427a34adc3
stepsen89 <stephanie.senoner@gmail.com>
2018-02-19 15:56:56 +0100

    iteration 1 and 2 done

--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -3,3 +3,10 @@
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive a string, doesn't return anything.
+
+export interface ToDoInterface {
+  tasks: Array<string>;
+  addTask(task: string);
+  listAllTasks();
+  deleteTask(task: string);
+}
