e41f74cf3abfcc39bbdd5d48954d6cd2ac03e809
Rodrigo Daoud <rodaoud93@gmail.com>
2018-02-19 15:56:26 +0100

    First Iteration

---
+++ starter-code/iteration-1/main.js
@@ -0,0 +1,26 @@
+// Iteration 1
+// 1. Create an array of strings
+var tasksArray = [];
+var counter = 0;
+// 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
+function addTask(task) {
+    tasksArray.push(task);
+    console.log("Task", task, "inserted in the list");
+    counter++;
+}
+// 3. Create a function to list all tasks, it must show in the console de task.
+function listAllTasks() {
+    tasksArray.forEach(function (e) {
+        console.log(e);
+    });
+}
+// 4. Create a function to delete a task, you must find the task inside the array and delete it.
+// Execution
+console.log("Number of items:", addTask('This is our first task'));
+console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
+listAllTasks();
+console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
+listAllTasks();
