d4310f988e13887f199ba5aa10913c0c4ef61150
Raul Merino <merinoyrr@gmail.com>
2018-03-05 13:39:50 -0600

    Done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,35 @@
 "use strict";
+
+// 1. Create a class Todo that implements the Interface created before.
+var Todo = /** @class */ (function () {
+    function Todo() {
+        this.myList = [];
+    }
+    Todo.prototype.addTask = function (task) {
+        this.myList.push(task);
+        console.log("=========== NEW TASK ===========\n    Task \"" + task + "\" inserted in the list\n    Number of items: " + this.myList.length);
+        return this.myList.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        this.myList.forEach(function (task) {
+            console.log(task);
+        });
+    };
+    Todo.prototype.deleteTask = function (task) {
+        this.myList.splice(this.myList.indexOf(task), 1);
+        console.log("=========== TASK REMOVED ===========\n    Task \"" + task + "\" removed from the list\n    Number of items: " + this.myList.length);
+        return this.myList.length;
+    };
+    return Todo;
+}());
+// Execution
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,7 +1,29 @@
 // Add the reference to the "TodoInterface"
-
+import {Task} from './todoInterface'
 // 1. Create a class Todo that implements the Interface created before.
+class Todo implements Task {
+    myList: Array<string>=[]
 
+    addTask(task: string): number{
+        this.myList.push(task)
+        console.log(`=========== NEW TASK ===========
+    Task "${task}" inserted in the list
+    Number of items: ${this.myList.length}`)
+        return this.myList.length
+    }
+    listAllTasks(): void{
+        this.myList.forEach((task)=>{
+            console.log(task)
+        })
+    }
+    deleteTask(task: string): number{
+        this.myList.splice(this.myList.indexOf(task), 1)
+        console.log(`=========== TASK REMOVED ===========
+    Task "${task}" removed from the list
+    Number of items: ${this.myList.length}`)
+        return this.myList.length
+    }
+  }
 // Execution
 let myTodos = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
