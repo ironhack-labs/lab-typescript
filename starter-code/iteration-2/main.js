b4702a12565f7820683e54585db60720108e5348
Pablo Gomez <gomezpa@bellsouth.net>
2018-03-05 11:52:06 -0500

    Done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,36 @@
 "use strict";
+exports.__esModule = true;
+var Todo = (function () {
+    function Todo() {
+        this.tasksList = [];
+    }
+    Todo.prototype.addTask = function (task) {
+        this.tasksList.push(task);
+        console.log("=========== NEW TASK ===========\n                " + task + "\n                ");
+        return this.tasksList.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        this.tasksList.forEach(function (task) {
+            console.log(task);
+        });
+    };
+    Todo.prototype.deleteTask = function (task) {
+        var deleteIndex = this.tasksList.indexOf(task);
+        if (deleteIndex !== -1) {
+            this.tasksList.splice(deleteIndex, 1);
+            console.log("=========== TASK REMOVED ===========\n                    " + task + "\n                    ");
+        }
+        return this.tasksList.length;
+    };
+    return Todo;
+}());
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
