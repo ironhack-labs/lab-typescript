48b25432be4bafe8568cf46ccfc1b95cb5603c9a
Guillermo Alarcón <xxmgaxx@gmail.com>
2018-03-06 19:46:09 +0100

    done

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,12 +1,25 @@
 // Iteration 1
 // 1. Create an array of strings
-
+let myTasks: Array<string> = []
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
+function addTask(task: string): number {
+  myTasks.push(task);
+  console.log("=========== NEW TASK ===========\ntask " + '"' + task + '"' + " inserted in the list");
+  return (myTasks.length);
+}
 // 3. Create a function to list all tasks, it must show in the console de task.
-
+function listAllTasks() {
+  console.log("=========== LIST TASKS ===========")
+  myTasks.forEach(function (elem) {
+    console.log(elem)
+  })
+}
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
-
+function deleteTask(task: string): number {
+  myTasks.splice(myTasks.indexOf(task), 1)
+  console.log("=========== TASK REMOVED ===========\ntask " + '"' + task + '"' + " removed from the list");
+  return (myTasks.length);
+}
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
 console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
--- starter-code/iteration-3/main.js
+++ starter-code/iteration-3/main.js
@@ -1,1 +1,63 @@
 "use strict";
+exports.__esModule = true;
+// Create class TodoItem that implements the corresponding interface
+var TodoItem = /** @class */ (function () {
+    function TodoItem(title) {
+        this.title = title;
+        this.status = false;
+        this.updatedAt = new Date();
+    }
+    TodoItem.prototype.toogleStatus = function () {
+        this.status === true ? this.status = false : this.status = true;
+    };
+    return TodoItem;
+}());
+// Create class TodoList that implements the corresponding interface
+var TodoList = /** @class */ (function () {
+    function TodoList() {
+        this.myTasks = [];
+    }
+    TodoList.prototype.addTask = function (task) {
+        this.myTasks.push(task);
+        console.log("\n=========== NEW TASK ===========");
+        return this.myTasks.length;
+    };
+    TodoList.prototype.listAllTasks = function () {
+        console.log("\n=========== LIST TASKS ===========");
+        this.myTasks.forEach(function (elem) {
+            console.log(elem.title);
+        });
+    };
+    TodoList.prototype.deleteTask = function (task) {
+        console.log("\n=========== TASK REMOVED ===========");
+        this.myTasks = this.myTasks.filter(function (item) {
+            return item === task;
+        });
+        return this.myTasks.length;
+    };
+    TodoList.prototype.listUncomplete = function () {
+        this.myTasks.forEach(function (elem) {
+            if (elem.status === false) {
+                console.log(elem.title);
+            }
+        });
+    };
+    return TodoList;
+}());
+// Execution
+var task1 = new TodoItem('This is our first task');
+var task2 = new TodoItem('Eat pizza 🍕 yummy!!!');
+var task3 = new TodoItem('Finish this iteration 1!! 🤓');
+var task4 = new TodoItem('Finish this iteration 2!! 🤓');
+var task5 = new TodoItem('Finish this iteration 3!! 🤓');
+var myTodos = new TodoList();
+console.log("Number of items:", myTodos.addTask(task1));
+console.log("Number of items:", myTodos.addTask(task2));
+console.log("Number of items:", myTodos.addTask(task3));
+console.log("Number of items:", myTodos.addTask(task4));
+console.log("Number of items:", myTodos.addTask(task5));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask(task3));
+console.log("Number of items:", myTodos.deleteTask(task4));
+console.log("Number of items:", myTodos.deleteTask(task5));
+myTodos.listUncomplete();
