7cdfc92a0ed9d01ba010833eabcd52b2407ff6a6
fredrik62 <hogstrom10@gmail.com>
2018-04-30 15:28:10 +0200

    half way done joana-fredrik-bcn

--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,9 +1,42 @@
 // Add the reference to the "TodoInterface"
 
+
+import { TodoInterface } from "./todoInterface";
 // 1. Create a class Todo that implements the Interface created before.
 
+class Todo implements TodoInterface {
+
+          constructor(public arrayOfTasks: Array<string>) {}
+
+          addTask(task:string): number {
+            this.arrayOfTasks.push(task);
+            console.log("=========== NEW TASK ===========");
+            console.log(`Task ${task} inserted in the list`);
+            console.log(`Number of items: ${this.arrayOfTasks.length}`);
+            return this.arrayOfTasks.length;
+          }
+           
+          listAllTasks(): void {
+
+            for (let i = 0 ; i < this.arrayOfTasks.length; i++) {
+              console.log(this.arrayOfTasks[i]);
+            }
+            
+          }
+              deleteTask(task:string): number {
+                this.arrayOfTasks = this.arrayOfTasks.filter(e => e !== task);
+                console.log("=========== REMOVE TASK ===========");
+                console.log(`Task ${task} removed from the list`);
+                console.log(`Number of items: ${this.arrayOfTasks.length}`);
+                return this.arrayOfTasks.length;
+          }
+
+
+}
+
+
 // Execution
-let myTodos = new Todo();
+let myTodos = new Todo(this.Todo);
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
