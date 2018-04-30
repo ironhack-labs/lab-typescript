89be4921af365239a19dcefb9c5c63073a7c8332
Yago Vega <yagofvv@gmail.com>
2018-04-30 12:35:18 +0200

    first iteration

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,18 +1,36 @@
 // Iteration 1
 // 1. Create an array of strings
-
+let array: Array<string> = [];
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
-// 3. Create a function to list all tasks, it must show in the console de task.
-
+let addTask = (task: string) => {
+  array.push(task);
+  console.log("Added task");
+  console.log(array.length);
+  return array.length;
+};
+// 3. Create a function to list all tasks, it must show in the console the task.
+let listAllTasks = () => {
+  array.forEach(e => {
+    console.log(e);
+  });
+};
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
-
+let deleteTask = (task: string) => {
+  array.forEach((e, i) => {
+    if (task === e) {
+      array.splice(i, 1);
+      console.log(`Deleted item ${task}`);
+    }
+  });
+  console.log(array.length);
+  return array.length;
+};
 // Execution
-console.log("Number of items:", addTask('This is our first task'));
-console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
-console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
-console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
+console.log("Number of items:", addTask("This is our first task"));
+console.log("Number of items:", addTask("Eat pizza 🍕 yummy!!!"));
+console.log("Number of items:", addTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", addTask("Finish this iteration 2!! 🤓"));
+console.log("Number of items:", addTask("Finish this iteration 3!! 🤓"));
 listAllTasks();
-console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", deleteTask("Finish this iteration 1!! 🤓"));
 listAllTasks();
