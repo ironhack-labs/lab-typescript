0908cdd5907a46dd02357a3033aa028a704e1e9b
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-05-14 13:08:07 -0400

    WIP. Iteration 3 in progress.

--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,9 +1,44 @@
 // Add the reference to the "TodoInterface"
+import TodoInterface from './todoInterface';
+
 
 // 1. Create a class Todo that implements the Interface created before.
+class Todo implements TodoInterface {
+  constructor(
+    public doArray: Array<string>,
+  ) {}
+
+  addTask(task:string):number {
+    this.doArray.push(task);
+    console.log(`
+    =========== NEW TASK ===========
+    Task ${task} inserted in the list
+    `)
+    return this.doArray.length
+  }
+
+  listAllTasks():void {
+    this.doArray.forEach(task => {
+      console.log(task)
+    })
+  }
+
+  deleteTask(task:string):number {
+    this.doArray.forEach((item, index) => {
+      if(task === item) {
+        this.doArray.splice(index, 1)
+      }
+    });
+    console.log(`
+    =========== TASK REMOVED ===========
+    Task ${task} removed from the list
+    `)
+    return this.doArray.length
+  }
+}
 
 // Execution
-let myTodos = new Todo();
+let myTodos = new Todo([]);
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
