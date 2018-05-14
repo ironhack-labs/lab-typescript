0442ac5fd14522f0952fea86fa560a7b6562cd35
KatAlderman <kat@uxkat.co>
2018-05-14 13:03:03 -0400

    done

--- starter-code/iteration-3/main.ts
+++ starter-code/iteration-3/main.ts
@@ -1,8 +1,51 @@
 // Add the reference to the interface
+import {TodoItemInterface, TodoListInterface} from './interfaces';
 
 // Create class TodoItem that implements the corresponding interface
+class TodoItem implements TodoItemInterface {
+  constructor (public title: string) {};
+  status: boolean = false;
+  updatedAt: Date;
+  toggleStatus(){
+    if (status) {
+      this.status = false;
+    } else {
+      this.status = true;
+    }
+    this.updatedAt = new Date();
+  }
+}
+
 
 // Create class TodoList that implements the corresponding interface
+class TodoList implements TodoListInterface {
+  constructor (public tasks: Array<TodoItemInterface> = []) {};
+  addTask(task){
+    this.tasks.push(task);
+    console.log('Added task');
+    console.log(this.tasks.length);
+    return this.tasks.length;
+  }
+
+
+  listAllTasks() {
+    this.tasks.forEach(task => {
+      task.updatedAt = new Date();
+      console.log(task.title, task.updatedAt);
+    });
+  }
+
+  deleteTask(task) {
+    var index = this.tasks.indexOf(task, 0);
+    if (index > -1) {
+    this.tasks.splice(index, 1);
+  }
+  console.log('Task deleted');
+  console.log(this.tasks.length);
+  return this.tasks.length;
+  }
+}
+
 
 // Execution
 let task1 = new TodoItem('This is our first task');
@@ -22,4 +65,4 @@ myTodos.listAllTasks();
 console.log("Number of items:", myTodos.deleteTask(task3));
 console.log("Number of items:", myTodos.deleteTask(task4));
 console.log("Number of items:", myTodos.deleteTask(task5));
-myTodos.listUncomplete();
+// myTodos.listUncomplete();
