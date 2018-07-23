9318e4458a7928902a01ba0af14a469edcdb969b
pabloturati <pablo.turati@gmail.com>
2018-07-23 13:48:53 -0500

    PP Terminado

--- starter-code/iteration-3/main.ts
+++ starter-code/iteration-3/main.ts
@@ -1,8 +1,59 @@
 // Add the reference to the interface
+import {TodoItemInterface} from "./interfaces"
+import {TodoListInterface} from "./interfaces"
 
 // Create class TodoItem that implements the corresponding interface
 
+class TodoItem implements TodoItemInterface {
+    status: boolean;
+    title: string;
+    updatedAt: Date
+
+    constructor(title: string){
+        this.title = title;
+        this.status = true;
+    }
+    myArray: Array<string>
+    
+    toggleStatus(): void{
+        if(this.status === true) this.status = false;
+        else this.status = true;
+    }
+}
+
 // Create class TodoList that implements the corresponding interface
+
+class TodoList implements TodoListInterface {
+    task: TodoItem
+    myArray: Array<TodoItem>
+
+    
+    addTask(task:TodoItem): number{
+        this.myArray.push(task);
+        console.log(task)
+        console.log(this.myArray.length)
+        return this.myArray.length;
+    }
+    listAllTasks(): void{
+        this.myArray.forEach(function(element){
+            console.log(element);
+        })
+    }
+    deleteTask(task:TodoItem): number{
+        //let lastElement = myArray.pop()
+        let indexOf = null;
+        this.myArray.forEach(function(element, index){
+            if(element === task){
+                this.myArray.splice(index, 1)
+            }
+        })
+        //array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+        //console.log(lastElement)
+        //console.log(myArray.length)
+        return this.myArray.length;
+    }
+
+}
 
 // Execution
 let task1 = new TodoItem('This is our first task');
@@ -22,4 +73,4 @@ myTodos.listAllTasks();
 console.log("Number of items:", myTodos.deleteTask(task3));
 console.log("Number of items:", myTodos.deleteTask(task4));
 console.log("Number of items:", myTodos.deleteTask(task5));
-myTodos.listUncomplete();
+// myTodos.listUncomplete();
