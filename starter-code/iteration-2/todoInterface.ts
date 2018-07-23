746cb865a4bbe1c1a470c4e8c044bfca8e3adc8b
Chad Barcelo <chadbarcelo@outlook.com>
2018-07-23 12:30:26 -0400

    done

--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -3,3 +3,22 @@
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive a string, doesn't return anything.
+
+// interface Todo {
+//   engines: number;
+//   wheels:  number;
+// }
+// Write the interface for class Todo. It must have:
+// - An array of strings as a property.
+// - Method for adding task where will receive a string, doesn't return anything.
+// - Method for listing all task in the console, doesn't return anything.
+// - Method for delete a task where will receive a string, doesn't return anything.
+
+export interface TodoInterface {
+  tasks : Array<string>;
+  addTask(task:string):number;
+  listAllTasks():void;
+  deleteTask(task:string):number;
+}
+
+
