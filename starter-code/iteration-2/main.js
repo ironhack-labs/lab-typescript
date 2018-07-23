408b91e34d2047621fe3ff486735eefa70481072
Rafael González Vázquez <rafaelg@lycklig.com.mx>
2018-07-23 12:48:14 -0500

    PP MEX - Rafael & Alberto

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,39 @@
 "use strict";
+// 1. Create a class Todo that implements the Interface created before.
+var Todo = (function () {
+    function Todo() {
+        this.tasks = [];
+    }
+    Todo.prototype.addTask = function (task) {
+        this.tasks.push(task);
+        return this.tasks.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        this.tasks.forEach(function (task) {
+            console.log(task);
+        });
+    };
+    Todo.prototype.deleteTask = function (task) {
+        var found = this.tasks.indexOf(task);
+        if (found < 0) {
+            console.log('no se encontó');
+            return this.tasks.length;
+        }
+        else {
+            var deleted = this.tasks.splice(found, 1);
+            return this.tasks.length;
+        }
+    };
+    return Todo;
+}());
+// Execution
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
