a5a9653d376d70e9c35d0d11f2325d57081aa31f
Felipe Vallina <fvallinalp@gmail.com>
2018-07-23 14:49:05 -0500

    done

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,34 @@
 "use strict";
+exports.__esModule = true;
+// Add the reference to the "TodoInterface"
+// 1. Create a class Todo that implements the Interface created before.
+var Todo = /** @class */ (function () {
+    function Todo() {
+        this.tasks = [];
+    }
+    Todo.prototype.addTask = function (task) {
+        this.tasks.push(task);
+        console.log("=========== NEW TASK =========== \n                Task " + task + " inserted in the list   \n                Number of items: " + task.length + "    \n                ");
+    };
+    Todo.prototype.listAllTasks = function () {
+        this.tasks.forEach = function (task) {
+            console.log(task);
+        };
+    };
+    Todo.prototype.deleteTask = function (tasks) {
+        this.tasks.splice(tasks.indexOf(tasks), 1);
+        console.log("=========== TASK REMOVED ===========\n        Task \"" + tasks + "\"\n        Number of items: " + Array.length);
+    };
+    return Todo;
+}());
+// Execution
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
--- starter-code/iteration-2/todoInterface.js
+++ starter-code/iteration-2/todoInterface.js
@@ -1,1 +1,5 @@
 "use strict";
+exports.__esModule = true;
+// - Method for adding task where will receive a string, doesn't return anything.
+// - Method for listing all task in the console, doesn't return anything.
+// - Method for delete a task where will receive a string, doesn't return anything.
