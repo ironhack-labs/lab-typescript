95af7ae181fbbbcd5722cf423af3745c90ca0143
CPBCPB <christinebeck2010@gmail.com>
2018-07-23 12:40:03 -0400

    MIA christine beck

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,12 +1,20 @@
 // Iteration 1
 // 1. Create an array of strings
-
+let tasks: string[]=[];
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
+function addTask(task:string){
+    tasks.push(task); 
+}
 // 3. Create a function to list all tasks, it must show in the console de task.
-
+function listAllTasks(){
+    tasks.forEach(eachTask => {
+        console.log(eachTask)
+    });
+}
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
-
+function deleteTask(task:string){
+    tasks.splice(tasks.indexOf(task), 1)
+}
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
 console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,33 @@
 "use strict";
+exports.__esModule = true;
+var Todo = /** @class */ (function () {
+    function Todo(tasks) {
+        if (tasks === void 0) { tasks = []; }
+        this.tasks = tasks;
+    }
+    Todo.prototype.addTask = function (task) {
+        this.tasks.push(task);
+        return this.tasks.length;
+    };
+    Todo.prototype.listAllTasks = function () {
+        this.tasks.forEach(function (eachTask) {
+            console.log(eachTask);
+        });
+    };
+    Todo.prototype.deleteTask = function (task) {
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+        return this.tasks.length;
+    };
+    return Todo;
+}());
+// Execution
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
