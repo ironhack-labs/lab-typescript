8ddc0a87b4389372f65dbcc96816ca1bf7c0a646
Anna Hogberg <am.hogberg@gmail.com>
2018-07-23 21:40:28 +0200

    iteracion 2

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,39 @@
 "use strict";
+exports.__esModule = true;
+// 1. Create a class Todo that implements the Interface created before.
+var Todo = /** @class */ (function () {
+    function Todo() {
+        this.taskList = [];
+    }
+    //2. Adds a task
+    Todo.prototype.addTask = function (task) {
+        this.taskList.push(task);
+        console.log("\n    =========== NEW TASK ===========\n    " + task + "\n    Number of items: " + this.taskList.length);
+        return this.taskList.length;
+    };
+    //3. List all Task
+    Todo.prototype.listAllTasks = function () {
+        var tasks = this.taskList.forEach(function (tasks) {
+            console.log("\n    =========== ALL TASKS ===========\n    " + tasks + "\n    ");
+        });
+    };
+    //4. Deletes a task
+    Todo.prototype.deleteTask = function (task) {
+        var n = this.taskList.indexOf(task);
+        this.taskList.splice(n, 1);
+        console.log("\n    =========== DELETED TASK ===========\n    " + task + "\n    Number of items: " + this.taskList.length + "\n    ");
+        return this.taskList.length;
+    };
+    return Todo;
+}());
+//Execution
+var myTodos = new Todo();
+console.log("Number of items:", myTodos.addTask("This is our first task"));
+console.log("Number of items:", myTodos.addTask("Eat pizza 🍕 yummy!!!"));
+console.log("Number of items:", myTodos.addTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", myTodos.addTask("Finish this iteration 2!! 🤓"));
+console.log("Number of items:", myTodos.addTask("Finish this iteration 3!! 🤓"));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", myTodos.deleteTask("Finish this iteration 2!! 🤓"));
+myTodos.listAllTasks();
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,15 +1,53 @@
 // Add the reference to the "TodoInterface"
-
+import { TodoInterface } from "./todoInterface";
 // 1. Create a class Todo that implements the Interface created before.
+class Todo implements TodoInterface {
+  taskList: Array<string> = [];
 
-// Execution
+  constructor() {}
+  //2. Adds a task
+  addTask(task: string): number {
+    this.taskList.push(task);
+    console.log(`
+    =========== NEW TASK ===========
+    ${task}
+    Number of items: ${this.taskList.length}`);
+    return this.taskList.length;
+  }
+
+  //3. List all Tasks
+  listAllTasks(){
+    let tasks = this.taskList.forEach(function (tasks){
+      console.log(`
+    =========== ALL TASKS ===========
+    ${tasks}
+    `);
+    })
+  }
+
+
+  //4. Deletes a task
+  deleteTask(task:string): number{
+    let n = this.taskList.indexOf(task);
+    this.taskList.splice(n, 1);
+    console.log(`
+    =========== DELETED TASK ===========
+    ${task}
+    Number of items: ${this.taskList.length}
+    `);
+    return this.taskList.length;
+  }
+
+}
+
+//Execution
 let myTodos = new Todo();
-console.log("Number of items:", myTodos.addTask('This is our first task'));
-console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+console.log("Number of items:", myTodos.addTask("This is our first task"));
+console.log("Number of items:", myTodos.addTask("Eat pizza 🍕 yummy!!!"));
+console.log("Number of items:", myTodos.addTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", myTodos.addTask("Finish this iteration 2!! 🤓"));
+console.log("Number of items:", myTodos.addTask("Finish this iteration 3!! 🤓"));
 myTodos.listAllTasks();
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", myTodos.deleteTask("Finish this iteration 2!! 🤓"));
 myTodos.listAllTasks();
