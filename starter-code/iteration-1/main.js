4dc7bddb43ba1b829a8b4b439be8e4ac8278f36e
Paula <paulagarizcun@gmail.com>
2018-07-23 12:20:32 +0200

    Iteracion 1

---
+++ starter-code/iteration-1/main.js
@@ -0,0 +1,32 @@
+// Iteration 1
+// 1. Create an array of strings
+var tasks = [];
+// 2. Create a function to add a task in to the array, call it `addTask`, 
+//the function has to accept a string and don't have to return anything.
+function addTask(task) {
+    tasks.push(task);
+    console.log("Task inserted");
+    return ("Number of items: " + tasks.length);
+}
+// 3. Create a function to list all tasks, it must show in the console de task.
+function listAllTasks() {
+    for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+        var task = tasks_1[_i];
+        console.log(task);
+    }
+}
+// 4. Create a function to delete a task, you must find the task inside the array and delete it.
+function deleteTask(task) {
+    tasks.splice(tasks.indexOf(task), 1);
+    console.log("Task delete");
+    return ("Number of items: " + tasks.length);
+}
+// Execution
+console.log("Number of items:", addTask('This is our first task'));
+console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
+listAllTasks();
+console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
+listAllTasks();
