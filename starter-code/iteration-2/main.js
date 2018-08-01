a15961d10b2700730743cf269cc4e05f2abf0652
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-08-01 18:54:04 -0400

    Edit tests

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -16,12 +16,12 @@ class Todo {
     }
 }
 let myTodos = new Todo([]);
-console.log("Number of items:", myTodos.addTask('This is our first task'));
-console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+console.log(myTodos.addTask('This is our first task'));
+console.log(myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log(myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log(myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log(myTodos.addTask('Finish this iteration 3!! 🤓'));
 myTodos.listAllTasks();
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+console.log(myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log(myTodos.deleteTask('Finish this iteration 2!! 🤓'));
 myTodos.listAllTasks();
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -23,12 +23,12 @@ class Todo implements TodoList {
 
 // Execution
 let myTodos = new Todo([]);
-console.log("Number of items:", myTodos.addTask('This is our first task'));
-console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
-console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+console.log(myTodos.addTask('This is our first task'));
+console.log(myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log(myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log(myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log(myTodos.addTask('Finish this iteration 3!! 🤓'));
 myTodos.listAllTasks();
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
-console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+console.log(myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log(myTodos.deleteTask('Finish this iteration 2!! 🤓'));
 myTodos.listAllTasks();
