5387b172b94bfdcc361f636ae144ee83b3ff3123
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-08-01 12:54:11 -0400

    Finish Iteration 2

--- starter-code/iteration-2/main.js
+++ starter-code/iteration-2/main.js
@@ -1,1 +1,27 @@
-"use strict";
+class Todo {
+    constructor(tasks) {
+        this.tasks = tasks;
+    }
+    addTask(task) {
+        this.tasks.push(task);
+    }
+    listAllTasks() {
+        this.tasks.forEach(task => {
+            console.log(task);
+        });
+    }
+    deleteTask(task) {
+        const taskIndex = this.tasks.indexOf(task);
+        this.tasks.splice(taskIndex, 1);
+    }
+}
+let myTodos = new Todo([]);
+console.log("Number of items:", myTodos.addTask('This is our first task'));
+console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
+console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
+myTodos.listAllTasks();
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
+console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
+myTodos.listAllTasks();
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,9 +1,28 @@
 // Add the reference to the "TodoInterface"
+import {TodoList} from './todoInterface';
 
 // 1. Create a class Todo that implements the Interface created before.
+class Todo implements TodoList {
+  constructor(public tasks: string[]) {}
+
+  addTask(task: string): void {
+    this.tasks.push(task);
+  }
+
+  listAllTasks(): void {
+    this.tasks.forEach(task => {
+      console.log(task);
+    })
+  }
+
+  deleteTask(task: string): void {
+    const taskIndex = this.tasks.indexOf(task);
+    this.tasks.splice(taskIndex, 1);
+  }
+}
 
 // Execution
-let myTodos = new Todo();
+let myTodos = new Todo([]);
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
