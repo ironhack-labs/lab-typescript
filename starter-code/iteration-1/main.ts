b4ad8f9281f00339739803a4e12ceed60888c173
mafeq03 <mafeq03@gmail.com>
2018-08-08 22:03:09 -0400

    Done

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,11 +1,26 @@
 // Iteration 1
 // 1. Create an array of strings
+let myArray: Array<string> = [];
 
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
+function addTask (task : string){
+  myArray.push(task);
+}
 // 3. Create a function to list all tasks, it must show in the console de task.
+function listAllTasks (array: Array<string>){
+  var i = 0
+  for (i; i < array.length; i++){
+    console.log(array[i]);
+  }
+}
 
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
+function deleteTask(array: Array<string>, task: string){
+  let index: number = array.indexOf(task);
+  array.splice(index, 1);
+  console.log(`${task} removed from the tasklist`);
+  return array.length;
+}
 
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
@@ -13,6 +28,7 @@ console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
 console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
 console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
-listAllTasks();
-console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
-listAllTasks();
+
+// listAllTasks();
+// deleteTask('Finish this iteration 1!! 🤓'));
+// listAllTasks();
--- starter-code/iteration-3/interfaces.js
+++ starter-code/iteration-3/interfaces.js
@@ -1,1 +1,39 @@
-"use strict";
+// Create class TodoItem that implements the corresponding interface
+var TodoItem = /** @class */ (function () {
+    function TodoItem(title) {
+        this.title = title;
+        this.title = title;
+        this.status = false;
+        this.updatedAt = new Date();
+    }
+    TodoItem.prototype.toggleStatus = function () {
+        return !this.status;
+    };
+    return TodoItem;
+}());
+// Create class TodoList that implements the corresponding interface
+var TodoList = /** @class */ (function () {
+    function TodoList() {
+        this.listItemElements = [];
+    }
+    TodoList.prototype.addTask = function (todoItem) {
+        this.listItemElements.push(todoItem);
+        console.log("Task " + todoItem.title + " added to the list");
+        return this.listItemElements.length;
+    };
+    ;
+    TodoList.prototype.listAllTasks = function () {
+        this.listItemElements.forEach(function (listItem) {
+            console.log(listItem.title, listItem.status, listItem.updatedAt);
+        });
+    };
+    ;
+    TodoList.prototype.deleteTask = function (todoItem) {
+        var position = this.listItemElements.indexOf(todoItem);
+        this.listItemElements.splice(position, 1);
+        console.log(todoItem.title + " removed from the list");
+        return this.listItemElements.length;
+    };
+    ;
+    return TodoList;
+}());
