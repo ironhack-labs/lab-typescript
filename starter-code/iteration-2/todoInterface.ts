d099c74bfd3107d10f522b37400a54cfbe5f1f64
Georgina Pacey <paceygeorgina@gmail.com>
2018-09-05 23:47:28 +0200

    iteration 2

--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -1,4 +1,10 @@
 // Write the interface for class Todo. It must have:
+export interface TodoInterface {
+    listItems: Array<string>;
+    addTask(item: string): number;
+    listAllTasks(): void;
+    deleteTask(item: string): number;
+}
 // - An array of strings as a property.
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
