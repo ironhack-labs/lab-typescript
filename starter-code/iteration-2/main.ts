4b4f45ba62ae163e3131331159cc70738cc2dedb
AlvaroCS <alvaro.corral@innocv.com>
2019-04-02 12:54:31 +0200

    It2

--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,9 +1,24 @@
 // Add the reference to the "TodoInterface"
+import ITodo from './todoInterface'
 
+class Todo implements ITodo {
+    tasks: string[];    
+    
+    addTask(taskToAdd: string): void {
+        this.tasks.push(taskToAdd);
+    }
+    listAllTasks(): void {
+        this.tasks.forEach(function (task) {
+            console.log("Taks " + task);
+        });
+    }
+    deleteTask(taskToDelete: string): void {
+        this.tasks = this.tasks.filter(function (task) { return task !== taskToDelete; });
+    }
+}
 // 1. Create a class Todo that implements the Interface created before.
-
 // Execution
-let myTodos = new Todo();
+let myTodos  = new Todo();
 console.log("Number of items:", myTodos.addTask('This is our first task'));
 console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
--- starter-code/iteration-2/todoInterface.ts
+++ starter-code/iteration-2/todoInterface.ts
@@ -3,3 +3,12 @@
 // - Method for adding task where will receive a string, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive a string, doesn't return anything.
+export default interface ITodo {
+    tasks: Array<string>
+
+    addTask(taskToAdd: string): void;
+    listAllTasks(): void;
+    deleteTask(taskToDelete: string): void;
+
+
+}
