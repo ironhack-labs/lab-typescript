787fa16de18c6d65c5101c70b93b2f2598bcf5bb
AlvaroCS <alvaro.corral@innocv.com>
2019-04-02 12:22:11 +0200

    Delete task (no this)

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -3,17 +3,17 @@
 let tasks: Array <string> =[]
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
 function addTask(task: string): void{
-    this.tasks.push(task)
+    tasks.push(task)
 }
 // 3. Create a function to list all tasks, it must show in the console de task.
 function listAllTasks():void {
-    this.tasks.forEach(task => {
+    tasks.forEach(task => {
         console.log(`Taks ${task}`);
     });
 }
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
 function deleteTask(taskToDelete):void{
-    this.tasks = this.tasks.filter(task => task !== taskToDelete)
+    tasks = tasks.filter(task => task !== taskToDelete)
 }
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
